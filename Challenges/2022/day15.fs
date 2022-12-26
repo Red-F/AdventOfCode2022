7ce36cdc7bfd15077c693fa55270666b32c65f9e
Fred Hoogduin <dev@hoogduin.net>
2022-12-26 17:18:50 +0100

    start using Map for sparse matrix

--- Challenges/2022/day15.fs
+++ Challenges/2022/day15.fs
@@ -13,26 +13,29 @@ type sparseMatrix() =
     // 'set' has two index values and a new value to place in the key's position
     and set (key1, key2) value = table[(key1, key2)] <- value
 
-type Boundary = { MaxX: int; MinX: int; MaxY: int; MinY: int; }
+type Boundary = { MaxX: int64; MinX: int64; MaxY: int64; MinY: int64 }
 
 let parseSegments lines =
   let step (s: string) =
     match s with
-    | Regex @"Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)" [sensorX; sensorY; beaconX; beaconY] -> [('S', int64 sensorX, int64 sensorY); ('B', int64 beaconX, int64 beaconY)]
+    | Regex @"Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)" [sensorX; sensorY; beaconX; beaconY] -> [(int64 sensorX, int64 sensorY), 'S'; (int64 beaconX, int64 beaconY), 'B']
     | _ -> failwith "mismatch"
   let beaconsAndSensors = Seq.map step lines
+  let theMap = beaconsAndSensors |> Seq.collect List.toSeq |> Map.ofSeq
+  let z = theMap[(8L, 7L)]
   let map = sparseMatrix()
-  beaconsAndSensors |> Seq.iter (fun l -> List.iter (fun (c, x, y) -> map[x, y] <- c) l)
-  map, beaconsAndSensors
+  beaconsAndSensors |> Seq.iter (fun l -> List.iter (fun ((x, y), c) -> map[x, y] <- c) l)
+  theMap, beaconsAndSensors
 
 let distance (sx, sy) (bx, by) =
   abs (sx - bx) + abs (sy - by)
 
-let writeMap (map: sparseMatrix) =
-  
-  for y in 0 .. (Array2D.length2 map) - 1 do
-    for x in 0 .. (Array2D.length1 map) - 1 do
-      Console.Write(map[x,y])
+let writeMap (map: Map<int64*int64,char>) =
+  let limits = fst (Map.minKeyValue map) |> (fun a ->  {MinX = fst a; MaxX = fst a; MinY = snd a; MaxY = snd a})
+  let limits = map |> Map.fold (fun acc (x, y) v -> {MinX = min x acc.MinX; MaxX = max x acc.MaxX; MinY = min y acc.MinY; MaxY = max y acc.MaxY}) limits
+  for y in limits.MinY .. limits.MaxY do
+    for x in limits.MinX .. limits.MaxX do
+      Console.Write(if map.ContainsKey (x,y) then map[x,y] else '.')
     Console.WriteLine()
     
 let solvePart1 data =
@@ -50,7 +53,8 @@ let solvePart1 data =
   // writeMap map
   // data
   // |> (fun (_, bas, _) -> Seq.map (fun (x: (char*int*int) list) -> distance (definitionToCoordinates x.Head) (definitionToCoordinates x.Tail.Head)) bas)
-  map
+  writeMap map
+  map 
   |> printfn "%A"
 
 let solvePart2 data =
